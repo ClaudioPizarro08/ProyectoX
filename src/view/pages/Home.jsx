5a81282ee23fec979d3fab570f3f1f9ed0efd6f8
ClaudioPizarro08 <claudio155219@hotmail.com>
2023-11-15 02:24:00 -0300

    Home a medias

--- src/view/pages/Home.jsx
+++ src/view/pages/Home.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const Home = () => {
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate('/register')
+  }
+
   return (
-    <h1>Home</h1>
+    <form>
+      <h1>Mantente organizado y eficiente con una plataforma robusta de gestión de tareas</h1>
+
+      <h2>Planifica, gestiona y monitorea tus tareas de una forma fácil y divertida</h2>
+
+      <img
+        src='Oficinatrabajando.png' alt='img de la pagina'
+        className='img-home'
+      />
+
+      <button onClick={handleClick}>Comenzar ahora</button>
+    </form>
   )
 }
