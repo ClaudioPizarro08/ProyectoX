82b7a5299b96f2beffbe4dd3e15b5a2554dda8f2
ClaudioPizarro08 <claudio155219@hotmail.com>
2023-11-15 14:55:19 -0300

    Table a medias

--- src/reducer/todos.js
+++ src/reducer/todos.js
@@ -7,6 +7,16 @@ export function reducer (state, action) {
     case 'LOGIN_USER':
       globalThis.localStorage.setItem('USER', JSON.stringify(action.payload))
       return { ...state, user: action.payload }
+    case 'LOGOUT':
+      return { ...state, user: null }
+    case 'LOAD_TASK':
+      return { ...state, tasks: action.payload }
+    case 'ADD_TASK':
+      return { ...state, tasks: [...state.tasks, action.payload] }
+    case 'UPDATE_TASK':
+      return { ...state, taskToUpdate: action.payload }
+    case 'DELETE_TASK':
+      return { ...state, tasks: state.tasks.filter(task => task._id !== action.payload) }
     default:
       return state
   }
--- src/view/pages/DashboardPage.jsx
+++ src/view/pages/DashboardPage.jsx
@@ -1,26 +1,53 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { initialState } from '../../reducer/todos'
 
 export const DashboardPage = () => {
+  const { state, dispatch } = useContext(TaskContext, initialState)
+
+  useEffect(() => {
+    fetch('https://birsbane-numbat-zjcf.1.us-1.fl0.io/api/todo?userId=}')
+      .then((response) => response.json())
+      .then((response) => {
+        dispatch({ type: 'LOAD_TASKS', payload: response.todos })
+      })
+  }, [])
+
+  const handleDelete = (taskId) => {
+    fetch('https://birsbane-numbat-zjcf.1.us-1.fl0.io/api/todo/' + state.tasks._id, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    )
+
+      .then(response => response.json())
+      .then(response => {
+        window.alert('Se eliminó correctamente la tarea ' + response.todo.name)
+        dispatch({ type: 'DELETE_TASK', payload: response.todo._id })
+      })
+  }
+
   return (
-    <form>
-      <h1>To do List</h1>
-      <input
-        type='text'
-        placeholder='Introduce la tarea'
-      />
-      <input
-        type='text'
-        placeholder='Descripción'
-      />
-      <input
-        type='text'
-        placeholder='Fecha DD/MM/AAAA'
-      />
-      <input
-        type='submit'
-        className='btn'
-        value='AGREGAR'
-      />
-    </form>
+    <>
+      <div className='task'>
+        <table className='table__container'>
+          <tbody>
+            {state.tasks.map((task) => (
+              <tr className='table__content' key={task.id}>
+                <td><p>{task.name}</p></td>
+                <td><p>{task.description}</p></td>
+                <td><p>{task.finishDate}</p></td>
+                <td><button onClick={() => dispatch({ type: 'UPDATE_TASK', payload: task })}>EditTask</button></td>
+                <td><button onClick={() => handleDelete(task._id)}>DeleteTask</button></td>
+                <td><button>TaskPending</button></td>
+                <td><button>TaskComplete</button></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+    </>
   )
 }
